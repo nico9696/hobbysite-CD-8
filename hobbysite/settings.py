a4a621c7979fc498e6c1921909890307273e86e1
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-05 15:12:47 +0800

    registered merchstore app in settings

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'merchstore',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- merchstore/models.py
+++ merchstore/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE, related_name="product_type")
     description = models.TextField()
-    price = price = models.DecimalField(decimal_places=2)  
+    price = models.DecimalField(max_digits=20, decimal_places=2)  
 
     # Ensures that product types display their id instead of "ProductType object (num)" in BOTH cmd line AND admin
     def __str__(self):
