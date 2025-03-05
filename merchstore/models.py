4ada5494f005c150cbfa2cf9bf73ba9d17403001
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-05 14:53:59 +0800

    made ProductType model

--- merchstore/models.py
+++ merchstore/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class ProductType(models.Model):
+    Name = models.CharField(max_length=255)
+    Description = models.TextField()
+
+    # Ensures that product types display their id instead of "ProductType object (num)" in BOTH cmd line AND admin
+    def __str__(self):
+        return str(self.id) 
