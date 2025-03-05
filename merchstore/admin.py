8a2ee560558aab5627ee29dbb7827e175ad9c3ed
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-05 15:07:47 +0800

    registered models in admin

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ProductType, Product
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    model = ProductType
+    search_fields = ('name', )
+    list_display = ('id', 'name')  # Shows readable names
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    search_fields = ('name', )
+    list_display = ('id', 'name')  # Shows readable names
 
 # Register your models here.
+admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(Product, ProductAdmin)
