09eabd5e785f6060effcb491c51a87b13f37d113
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-05 16:23:22 +0800

    modified views to account for parameter for product details

--- merchstore/views.py
+++ merchstore/views.py
@@ -4,5 +4,5 @@ from .models import ProductType, Product
 def show_products_list(request):
     return render(request, "merchstore/product_list.html", {"product_list": Product.objects.all()})
 
-def show_product_detail(request):
-    return render(request, "merchstore/product_detail.html", {"product": Product.objects.all()})
+def show_product_detail(request, num):
+    return render(request, "merchstore/product_detail.html", {"product": Product.objects.filter(product=num)})
