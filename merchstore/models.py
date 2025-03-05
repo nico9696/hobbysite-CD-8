84abe5025fc7f80d6c3411e89a99d0cfd618b78c
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-05 20:45:11 +0800

    added functionality to sort product and product type alphabetically

--- merchstore/models.py
+++ merchstore/models.py
@@ -8,8 +8,14 @@ class ProductType(models.Model):
     def __str__(self):
         return str(self.name) 
     
+    class Meta:
+        ordering = ['name']  # Orders ProductType alphabetically by name
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE, related_name="products")
     description = models.TextField()
     price = models.DecimalField(max_digits=20, decimal_places=2)  
+
+    class Meta:
+        ordering = ['name']  # Orders Product alphabetically by name
