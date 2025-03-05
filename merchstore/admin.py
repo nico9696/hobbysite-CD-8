901feacca3cb314794a6479158d4b8bb6eb0bf12
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-05 15:45:12 +0800

    added functionality to show product and product type alphabetically in admin

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -9,11 +9,13 @@ class ProductTypeAdmin(admin.ModelAdmin):
     model = ProductType
     search_fields = ('name', )
     list_display = ('id', 'name', 'description')  # Shows these readably
+    ordering = ('name',)  # Orders ProductType alphabetically by name
 
 class ProductAdmin(admin.ModelAdmin):
     model = Product
     search_fields = ('name', )
     list_display = ('id', 'name', 'product_type', 'description', 'price')  # Shows these readably
+    ordering = ('name',)  # Orders ProductType alphabetically by name
 
 # Register your models here.
 admin.site.register(ProductType, ProductTypeAdmin)
