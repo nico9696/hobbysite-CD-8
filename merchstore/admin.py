d0a6d1504b98beba1fc36969ba9f98f33130f2dd
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-05 15:43:00 +0800

    made in-line editing for admin

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from .models import ProductType, Product
 
+# can add product type straight from product table
+class ProductInline(admin.TabularInline):  
+    model = Product
+
 class ProductTypeAdmin(admin.ModelAdmin):
     model = ProductType
     search_fields = ('name', )
-    list_display = ('id', 'name')  # Shows readable names
+    list_display = ('id', 'name', 'description')  # Shows these readably
 
 class ProductAdmin(admin.ModelAdmin):
     model = Product
     search_fields = ('name', )
-    list_display = ('id', 'name')  # Shows readable names
+    list_display = ('id', 'name', 'product_type', 'description', 'price')  # Shows these readably
 
 # Register your models here.
 admin.site.register(ProductType, ProductTypeAdmin)
--- merchstore/models.py
+++ merchstore/models.py
@@ -4,16 +4,12 @@ class ProductType(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
-    # Ensures that product types display their id instead of "ProductType object (num)" in BOTH cmd line AND admin
+    # Ensures that product types are displayed by their names (instead of IDs) in admin
     def __str__(self):
-        return str(self.id) 
+        return str(self.name) 
     
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE, related_name="product_type")
+    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE, related_name="products")
     description = models.TextField()
     price = models.DecimalField(max_digits=20, decimal_places=2)  
-
-    # Ensures that product types display their id instead of "ProductType object (num)" in BOTH cmd line AND admin
-    def __str__(self):
-        return str(self.id) 
