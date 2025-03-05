e93c1c412377a70cdfbd15fa3b65aee210833b8a
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-05 16:14:48 +0800

    created views

--- merchstore/views.py
+++ merchstore/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import ProductType, Product
 
-def show_product_types(request):
+def show_products_list(request):
     return render(request, "merchstore/product_list.html", {"product_list": Product.objects.all()})
+
+def show_product_detail(request):
+    return render(request, "merchstore/product_detail.html", {"product": Product.objects.all()})
