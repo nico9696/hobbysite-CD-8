b5473639e5799fc277603c325783371349823380
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-05 16:21:43 +0800

    added views to urls.py in the app

---
+++ merchstore/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("items", views.show_products_list, name="show_products_list"), 
+    path("item/<int:num>", views.show_product_detail, name="show_product_detail"),
+]
