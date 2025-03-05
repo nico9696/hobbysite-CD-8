75418dbdcd800266c78eec37f0bc0a05ed25d29f
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-05 16:11:47 +0800

    modified product type template to be template for product list instead

--- merchstore/views.py
+++ merchstore/views.py
@@ -1,2 +1,5 @@
 from django.shortcuts import render
 from .models import ProductType, Product
+
+def show_product_types(request):
+    return render(request, "merchstore/product_list.html", {"product_list": Product.objects.all()})
