9389c32822b64d9f4f696c6e447bb21c251d1ddd
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-05 20:55:10 +0800

    updated views.py to sort products by type

--- merchstore/views.py
+++ merchstore/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from .models import Product
+from .models import ProductType, Product
 
 def show_products_list(request):
-    return render(request, "merchstore/products_list.html", {"product_list": Product.objects.all()})
+    return render(request, "merchstore/products_list.html", {
+        "product_type_list": ProductType.objects.all(),
+        "product_list": Product.objects.all()
+        })
 
 def show_product_details(request, num):
     return render(request, "merchstore/product_details.html", {"product": Product.objects.filter(id=num)})
