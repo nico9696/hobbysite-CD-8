d7e6fbc36f62f48c2b2889d34ae907c7fbf28939
nico9696 <nicholasbenedictdysy@gmail.com>
2025-03-05 17:00:35 +0800

    fixed xome typos that caused errors

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("items", views.show_products_list, name="show_products_list"), 
-    path("item/<int:num>", views.show_product_detail, name="show_product_detail"),
+    path("item/<int:num>", views.show_product_details, name="show_product_details"),
 ]
--- merchstore/views.py
+++ merchstore/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from .models import ProductType, Product
+from .models import Product
 
 def show_products_list(request):
-    return render(request, "merchstore/product_list.html", {"product_list": Product.objects.all()})
+    return render(request, "merchstore/products_list.html", {"product_list": Product.objects.all()})
 
-def show_product_detail(request, num):
-    return render(request, "merchstore/product_detail.html", {"product": Product.objects.filter(product=num)})
+def show_product_details(request, num):
+    return render(request, "merchstore/product_details.html", {"product": Product.objects.filter(id=num)})
