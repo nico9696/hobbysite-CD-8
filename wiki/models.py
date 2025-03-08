34cd86b177715885d770206f6c5f8f9d1bc3db0d
M4L3N4B <caikelvin0315@gmail.com>
2025-03-08 22:53:35 +0800

    Initial setup for models, views, urls

--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+'''
+TO-DO: Add __str__() and get_absolute_url() methods
+     : Implement sorting feature
+'''
+
+# Model for a category of articles
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+# Model for an article under a category
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        ArticleCategory,
+        null=True, # Allows articles to be created with a null category 
+        on_delete=models.SET_NULL # Becomes null upon deletion of the category
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True) # Set only once upon article creation
+    updated_on = models.DateTimeField(auto_now=True) # Set every time the article is edited
+    
+    
--- wiki/views.py
+++ wiki/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Article
 
-# Create your views here.
+
+def article_list(request):
+    articles = Article.objects.all()
+    ctx = {'articles': articles}
+    return render(request, 'article_list.html', ctx)
+
+
+def article_detail(request, article_id):
+    article = Article.objects.get(id=article_id)
+    ctx = {'article': article}
+    return render(request, 'article_detail.html', ctx)
