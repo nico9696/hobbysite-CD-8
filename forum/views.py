91af2e32166124623eea64b80da596a4919a1d3a
SchonnMike <schonnmike@gmail.com>
2025-03-10 23:21:31 +0800

    changed to group single unique thread per category

--- forum/views.py
+++ forum/views.py
@@ -2,14 +2,29 @@ from django.shortcuts import render
 from .models import PostCategory, Post
 
 def forum_list(request):
+    categories_with_threads = {} 
+
+    # loop through all category and get posts for each
+    for category in PostCategory.objects.all():
+        posts_in_category = Post.objects.filter(category=category).order_by('created_on')
+
+        unique_threads = {} # dictionary for unqiue thread titles within category
+        for post in posts_in_category:
+            if post.title not in unique_threads:
+                unique_threads[post.title] = post.id
+
+        categories_with_threads[category] = unique_threads
+
     ctx = {
-        'categories': PostCategory.objects.all(),
-        'posts': Post.objects.all()
+        'categories_with_threads': categories_with_threads
     }
     return render(request, 'forum_list.html', ctx)
 
 def forum_detail(request, forum_id):
+    post = Post.objects.get(id=forum_id)
+
     ctx = {
-        'post': Post.objects.get(id=forum_id)
+        'thread_title': post.title,
+        'posts': Post.objects.filter(title=post.title).order_by('-created_on')
     }
     return render(request, 'forum_detail.html', ctx)
