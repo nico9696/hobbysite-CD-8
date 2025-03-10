fc299fa87625be3b064f1e58bae96ff4b15b7cb9
SchonnMike <schonnmike@gmail.com>
2025-03-10 14:44:34 +0800

    added forum list and forum detail view

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import PostCategory, Post
 
-# Create your views here.
+def forum_list(request):
+    ctx = {
+        'categories': PostCategory.objects.all(),
+        'posts': Post.objects.all()
+    }
+    return render(request, 'forum_list.html', ctx)
+
+def article_detail(request, forum_id):
+    ctx = {
+        'forum': Post.objects.get(id=forum_id)
+    }
+    return render(request, 'forum_detail.html', ctx)
