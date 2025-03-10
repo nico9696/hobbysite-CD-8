fe94ff8c3354735fd3935405ca5e370a03d79f74
SchonnMike <schonnmike@gmail.com>
2025-03-10 15:02:30 +0800

    added models for admin

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import PostCategory, Post
 
-# Register your models here.
+class PostCategoryAdmin(admin.ModelAdmin):
+    model = PostCategory
+    search_fields = ('name', )
+    list_display = ('name', )
+
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    search_fields = ('title', 'category__name', )
+    list_display = ('title', 'category', 'created_on', 'updated_on')
+    
+    
+admin.site.register(PostCategory, PostCategoryAdmin)
+admin.site.register(Post, PostAdmin)
