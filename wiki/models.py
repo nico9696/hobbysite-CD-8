9de37158484105ca643e3506390abff23acc1a9a
M4L3N4B <caikelvin0315@gmail.com>
2025-03-10 13:44:49 +0800

    Added structure to article list template

--- wiki/models.py
+++ wiki/models.py
@@ -14,6 +14,7 @@ class ArticleCategory(models.Model):
     class Meta:
         ordering = ['name']
 
+
 # Model for an article under a category
 class Article(models.Model):
     title = models.CharField(max_length=255)
