628203a09a7659361ff88cd01cbf7502d04d9a37
SchonnMike <schonnmike@gmail.com>
2025-03-11 18:23:48 +0800

    removed redundant forum app url

--- forum/urls.py
+++ forum/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('forum/threads/', views.forum_list, name='forum_list'),
-    path('forum/thread/<int:forum_id>/', views.forum_detail, name='forum_detail'),
+    path('threads/', views.forum_list, name='forum_list'),
+    path('thread/<int:forum_id>/', views.forum_detail, name='forum_detail'),
 ]
