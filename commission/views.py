9ccc5226774c101d2629b1bdb22ea78508e3d221
EnsoSantos <162356579+EnsoSantos@users.noreply.github.com>
2025-03-12 11:42:20 +0800

    created views

--- commission/views.py
+++ commission/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Commission, Comment
 
-# Create your views here.
+def commission_list(request):
+    return render(request, 'commission/commission_list.html', {"commission_list": Commission.objects.all()})
+
+def commission_detail(request, commission_id):
+    commission = Commission.objects.filter(id=commission_id).first()  
+    comments = Comment.objects.filter(commission=commission)
+    return render(request, 'commission/commission_detail.html', {"commission": commission, "comments": comments})
