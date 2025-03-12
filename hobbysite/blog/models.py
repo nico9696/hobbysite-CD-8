5f8d6d9b3b6cb4e805a456d2c30e1b4a0fa47c66
iamgalata <gaetanojrscarlata@gmail.com>
2025-03-12 09:06:37 +0800

    created ArticleCategory models

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
