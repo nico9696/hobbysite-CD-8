038e4c6204263f93dbf82053699a65d5f7fbc024
EnsoSantos <162356579+EnsoSantos@users.noreply.github.com>
2025-03-12 16:36:38 +0800

    fixed import admin in urls

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path
 from commission import views
 
 urlpatterns = [
-    path('commissions/list/', views.commission_list, name='commission_list'),
+    path('admin/', admin.site.urls),
+    path('commission/list/', views.commission_list, name='commission_list'),
     path('commission/detail/<int:commission_id>/', views.commission_detail, name='commission_detail'),
 ]
