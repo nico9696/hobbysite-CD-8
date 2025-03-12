bee2191cf09309b1fea3c42e2c214171268c327a
EnsoSantos <162356579+EnsoSantos@users.noreply.github.com>
2025-03-12 16:18:32 +0800

    fixed import views to urls

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from commission import views
 
 urlpatterns = [
-    path('commissions/list', views.commission_list, name='commission_list'),
-     path('commission/detail/<int:commission_id>/', views.commission_detail, name='commission_detail'),
+    path('commissions/list/', views.commission_list, name='commission_list'),
+    path('commission/detail/<int:commission_id>/', views.commission_detail, name='commission_detail'),
 ]
