84efa3e6fa7247a83bd5ba4a1f095b3397bd3076
EnsoSantos <162356579+EnsoSantos@users.noreply.github.com>
2025-03-12 17:51:04 +0800

    fixed app name and migrate

--- commissions/apps.py
+++ commissions/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CommissionConfig(AppConfig):
+class CommissionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'commission'
+    name = 'commissions'
--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from commission import views
+from commissions import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('commission/list/', views.commission_list, name='commission_list'),
-    path('commission/detail/<int:commission_id>/', views.commission_detail, name='commission_detail'),
+    path('commissions/list/', views.commission_list, name='commission_list'),
+    path('commissions/detail/<int:commission_id>/', views.commission_detail, name='commission_detail'),
 ]
