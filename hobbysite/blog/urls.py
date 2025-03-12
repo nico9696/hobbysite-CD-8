449bbb5b20786aecf3bd5e444ca24866204f42fc
iamgalata <gaetanojrscarlata@gmail.com>
2025-03-12 09:32:47 +0800

    added url names blog/ and articles/

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.article_list, name="article_list"),
+    path('articles/', views.article_list, name='article_list'),
 ]
