8524b6ce21e74c1d4b9ca77ef67de756e2e6bd6f
iamgalata <gaetanojrscarlata@gmail.com>
2025-03-12 10:02:15 +0800

    edited article_list template to have classbased views

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+#from . import views
+from .views import article_list
 
 urlpatterns = [
-    path('articles/', views.article_list, name='article_list'),
+    ##path('articles/', views.article_list, name='article_list'),
+    path('articles/', article_list.as_view(), name="article_list")
 ]
--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Article
 
-def article_list(request):
-    return render(request, 'blog/article_list.html', {})
+#def article_list(request):
+#    return render(request, 'blog/article_list.html', {})
+
+class article_list(ListView):
+    model = Article
+    template_name = 'article_list.html'
+    context_object_name = 'articles'
+    ordering = ['-created_on']
+
