8ccbc2241f11c9ec09a0fea438b00f148b6e08d3
iamgalata <gaetanojrscarlata@gmail.com>
2025-03-12 11:01:45 +0800

    created article_details template and implemented links

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -5,9 +5,13 @@ from .models import Article
 #def article_list(request):
 #    return render(request, 'blog/article_list.html', {})
 
-class article_list(ListView):
+class ArticleList(ListView):
     model = Article
-    template_name = 'article_list.html'
+    template_name = 'blog/article_list.html'
     context_object_name = 'articles'
     ordering = ['-created_on']
 
+class ArticleDetails(DetailView):
+    model = Article 
+    template_name = 'blog/article_details.html'
+    context_object_name = 'article'
