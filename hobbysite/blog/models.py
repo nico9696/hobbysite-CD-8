b93953f9b344adbcb2a5128aec135911402c60b8
iamgalata <gaetanojrscarlata@gmail.com>
2025-03-12 09:10:08 +0800

    created Article models

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -11,3 +11,17 @@ class ArticleCategory(models.Model):
     def __str__(self):
         return self.name
 
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        ArticleCategory, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
