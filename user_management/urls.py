8a13defe1d02c1eb5a46b5db2c0d9bd6f9866ace
nico9696 <nicholasbenedictdysy@gmail.com>
2025-05-01 23:13:04 +0800

    added password reset functionality in urls

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path("login/", views.login_view, name="login"),
@@ -7,4 +9,9 @@ urlpatterns = [
     path("home/", views.home, name="home"),
     path("register/", views.register, name="register"),
     path("reset/", views.reset, name="reset"),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name ='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name ='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name ='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name ='password_reset_complete'),
 ]
