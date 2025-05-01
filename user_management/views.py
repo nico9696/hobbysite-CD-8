59a6641710e605f53d73f9fe0b85b8a7ca71341f
nico9696 <nicholasbenedictdysy@gmail.com>
2025-05-01 19:05:03 +0800

    added views.py

--- user_management/views.py
+++ user_management/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("recipe_list"))
+    return render(request, "profile/login.html")
+
+def logout_view(request):
+    logout(request)
+    return render(request, "profile/login.html")
