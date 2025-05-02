f37e34e7747b61de0f95f5708a2d74d4a778cd28
nico9696 <nicholasbenedictdysy@gmail.com>
2025-05-02 18:05:43 +0800

    configured urls and template for adding products

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("items", views.show_products_list, name="show_products_list"), 
-    path("item/<int:num>", views.show_product_details, name="show_product_details"),
+    path("items/", views.show_products_list, name="show_products_list"), 
+    path("item/<int:num>/", views.show_product_details, name="show_product_details"),
+    path("item/add/", views.add_recipe_and_ingredient, name="add_recipe_and_ingredient"),
 ]
