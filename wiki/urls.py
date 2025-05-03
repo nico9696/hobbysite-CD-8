3c50e73df97d76ca85641e8e3d7183f58b4d6fbf
M4L3N4B <caikelvin0315@gmail.com>
2025-05-03 19:05:42 +0800

    Fixed error in fetching article_list url in article_detail

--- wiki/urls.py
+++ wiki/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('articles/', views.article_list, name='wiki_list'),
+    path('articles/', views.article_list, name='wiki_list'), # Name written like this so that it does not clash with blog app's
     path('article/<int:article_id>/', views.article_detail, name='article_detail'),
     path('article/add/', views.article_add, name='article_add'),
     path('article/<int:article_id>/edit/', views.article_edit, name='article_edit'),
