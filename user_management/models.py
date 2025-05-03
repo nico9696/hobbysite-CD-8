716b48b96dc97505eec3cde9334de1e071ff8305
M4L3N4B <caikelvin0315@gmail.com>
2025-05-03 18:27:29 +0800

    Fixed issue with links not appearing in article_list

--- user_management/models.py
+++ user_management/models.py
@@ -7,3 +7,6 @@ class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	display_name = models.CharField(max_length=63)
 	email_address = models.EmailField(max_length=200)
+
+	def __str__(self):
+		return self.display_name
