1dd4a277fb2d3a4b12acf897139e9e7b50532723
EnsoSantos <162356579+EnsoSantos@users.noreply.github.com>
2025-05-05 22:34:19 +0800

    updated admin

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,17 +1,23 @@
 from django.contrib import admin
-from .models import Commission, Comment
+from .models import Commission, Job, JobApplication
 
 class CommissionAdmin(admin.ModelAdmin):
      model = Commission
      search_fields = ('title', )
-     list_display = ('id', 'title','description', 'created_on', 'updated_on', 'people_required')
+     list_display = ('id', 'title','description', 'created_on', 'updated_on', 'status')
  
-class CommentAdmin(admin.ModelAdmin):
-     model = Comment
-     search_fields = ('entry', 'commission__title')
-     list_display = ('id', 'entry', 'commission', 'created_on', 'updated_on')
+class JobAdmin(admin.ModelAdmin):
+     model = Job
+     search_fields = ('entry', 'role', 'commission__title')
+     list_display = ('id', 'entry', 'commission', 'role', 'people_required', 'created_on', 'updated_on', 'status')
      list_filter = ('commission', )
 
+class JobApplicationAdmin(admin.ModelAdmin):
+     model = JobApplication
+     search_fields = ('job', 'applicant')
+     list_display = ('id', 'job', 'commission', 'applied_on', 'applicant', 'status')
+     list_filter = ('commission', )
  
 admin.site.register(Commission, CommissionAdmin)
-admin.site.register(Comment, CommentAdmin)
+admin.site.register(Job, JobAdmin)
+admin.site.register(JobApplication, JobApplicationAdmin)
