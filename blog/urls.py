ecaa65ea727a6c551b87b3db9e61a1bbb3b16419
iamgalata <gaetanojrscarlata@gmail.com>
2025-05-06 18:02:12 +0800

    updated urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ArticleList, ArticleDetails
+from .views import (ArticleList,ArticleDetails,ArticleCreate,ArticleUpdate,)
 
 urlpatterns = [
     path('articles/', ArticleList.as_view(), name="blog_list"),
     path('article/<int:pk>/', ArticleDetails.as_view(), name="article_details"),
+    path('article/add/', ArticleCreate.as_view(), name="article_create"),
+    path('article/<int:pk>/edit/', ArticleUpdate.as_view(), name="article_update"),
 ]
