79748c03239a308d55871aafbf73de5894081c6c
iamgalata <gaetanojrscarlata@gmail.com>
2025-05-06 18:09:15 +0800

    updated admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import ArticleCategory, Article
+from .models import ArticleCategory, Article, Comment
 
-admin.site.register(ArticleCategory)
-admin.site.register(Article)
+@admin.register(ArticleCategory)
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'created_on', 'updated_on')
+    list_filter = ('category', 'author', 'created_on')
+    search_fields = ('title', 'entry')
+    raw_id_fields = ('author',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'article', 'created_on', 'updated_on')
+    list_filter = ('created_on', 'author')
+    search_fields = ('entry',)
+    raw_id_fields = ('author', 'article')
