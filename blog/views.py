e6b3b73e7b40393d4d3a612c0c03282dc81eaf1b
iamgalata <gaetanojrscarlata@gmail.com>
2025-05-06 17:48:30 +0800

    updated models.py to have author and a comment model

--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,27 @@ class ArticleList(ListView):
     model = Article
     template_name = 'blog/article_list.html'
     context_object_name = 'articles'
-    ordering = ['-created_on']
 
     def get_queryset(self):
-        return Article.objects.filter(category__isnull=False)
+        if self.request.user.is_authenticated:
+            user_articles = Article.objects.filter(author__user=self.request.user)
+            all_articles = Article.objects.exclude(author__user=self.request.user)
+        else:
+            user_articles = Article.objects.none()
+            all_articles = Article.objects.all()
+        return {
+            'user_articles': user_articles,
+            'grouped_articles': ArticleCategory.objects.prefetch_related('article_set'),
+            'all_articles': all_articles
+        }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        querysets = self.get_queryset()
+        context['user_articles'] = querysets['user_articles']
+        context['grouped_articles'] = querysets['grouped_articles']
+        context['all_articles'] = querysets['all_articles']
+        return context
 
 class ArticleDetails(DetailView):
     model = Article 
