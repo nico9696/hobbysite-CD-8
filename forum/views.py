f8cb5a91c6404dc223aa280fbaa2ec1e801abc01
SchonnMike <schonnmike@gmail.com>
2025-05-07 22:42:23 +0800

    fixed forum_list category, thread_create add thread and category, and thread_update

--- forum/views.py
+++ forum/views.py
@@ -1,37 +1,36 @@
 from django.shortcuts import render, redirect
 from .models import ThreadCategory, Thread, Comment
 from django.contrib.auth.decorators import login_required
-from .forms import ThreadForm, CommentForm
+from .forms import ThreadForm, CommentForm, ThreadCategoryForm
 
 def forum_list(request):
-    user_threads = {}
-    categories_with_threads = {}
+    user_threads_by_category = {}
+    other_threads_by_category = {}
 
-    # threads created by user
     if request.user.is_authenticated:
-        user_threads_qs = Thread.objects.filter(author=request.user.profile)
-        user_threads = {thread.title: thread.id for thread in user_threads_qs}
+        profile = request.user.profile
 
-    # threads by others (grouped by category)
-    # loop through all category and get threads for each
-    for category in ThreadCategory.objects.all():
-        threads_in_category = Thread.objects.filter(category=category).exclude(author=request.user.profile if request.user.is_authenticated else None).order_by('title')
+        # loop through all category, filter (by user or other), and get threads for each
+        for category in ThreadCategory.objects.all():
+            user_threads = Thread.objects.filter(author=profile, category=category).order_by('title')
+            other_threads = Thread.objects.filter(category=category).exclude(author=profile).order_by('title')
 
-        unique_threads = {} # dictionary for unqiue thread titles within category
-        for thread in threads_in_category:
-            if thread.title not in unique_threads:
-                unique_threads[thread.title] = thread.id
-
-        categories_with_threads[category] = unique_threads
+            user_threads_by_category[category] = {t.title: t.id for t in user_threads}
+            other_threads_by_category[category] = {t.title: t.id for t in other_threads}
+    else:
+        for category in ThreadCategory.objects.all():
+            other_threads = Thread.objects.filter(category=category).order_by('title')
+            other_threads_by_category[category] = {t.title: t.id for t in other_threads}
 
     ctx = {
-        'user_threads': user_threads,
-        'categories_with_threads': categories_with_threads
+        'user_threads_by_category': user_threads_by_category,
+        'other_threads_by_category': other_threads_by_category,
     }
     return render(request, 'forum/forum_list.html', ctx)
 
 def forum_detail(request, forum_id):
     thread = Thread.objects.get(id=forum_id)
+    related_threads = Thread.objects.filter(category=thread.category).exclude(id=thread.id)[:3]
     comments = Comment.objects.filter(thread=thread)
 
     # Handle CommentForm
@@ -46,29 +45,39 @@ def forum_detail(request, forum_id):
     else:
         form = CommentForm()
 
-    related_threads = Thread.objects.filter(category=thread.category).exclude(id=thread.id)[:2]
-
     ctx = {
         'thread': thread,
+        'related_threads': related_threads,
         'comments': comments,
         'form': form,
-        'related_threads': related_threads,
     }
     return render(request, 'forum/forum_detail.html', ctx)
 
 @login_required
 def thread_create(request):
+
     if request.method == 'POST':
-        form = ThreadForm(request.POST, request.FILES)
-        if form.is_valid():
-            new_thread = form.save(commit=False)
-            new_thread.author = request.user.profile
-            new_thread.save()
-            return redirect(new_thread.get_absolute_url())
+        if 'add_thread' in request.POST:
+            form = ThreadForm(request.POST, request.FILES)
+            if form.is_valid():
+                new_thread = form.save(commit=False)
+                new_thread.author = request.user.profile
+                new_thread.save()
+                return redirect('forum_detail', forum_id=new_thread.id)
+        elif 'add_category' in request.POST:
+            category_form = ThreadCategoryForm(request.POST)
+            if category_form.is_valid():
+                new_category = category_form.save()
+                return redirect('forum_list')
     else:
         form = ThreadForm()
-
-    return render(request, 'forum/thread_form.html', {'form': form})
+        category_form = ThreadCategoryForm()
+    
+    ctx = {
+        'form': form,
+        'category_form': category_form,
+    }
+    return render(request, 'forum/thread_create.html', ctx)
 
 @login_required
 def thread_update(request, forum_id):
@@ -76,14 +85,18 @@ def thread_update(request, forum_id):
 
     # Only allow to edit by thread author
     if thread.author != request.user.profile:
-        return redirect('forum_detail', forum_id=forum_id)
+        return redirect('forum_detail', forum_id=thread.id)
 
     if request.method == 'POST':
         form = ThreadForm(request.POST, request.FILES, instance=thread)
         if form.is_valid():
             form.save()
-            return redirect(thread.get_absolute_url())
+            return redirect('forum_detail', forum_id=thread.id)
     else:
         form = ThreadForm(instance=thread)
         
-    return render(request, 'forum/thread_form.html', {'form': form})
+    ctx = {
+        'form': form,
+        'thread': thread,
+    }
+    return render(request, 'forum/thread_update.html', ctx)
