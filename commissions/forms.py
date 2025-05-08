42382f1cb9f5de8bf480a020f447e39b231b864f
EnsoSantos <162356579+EnsoSantos@users.noreply.github.com>
2025-05-09 07:33:49 +0800

    created forms

---
+++ commissions/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Commission, Job, JobApplication
+
+
+class CommissionForm(forms.ModelForm):
+    class Meta:
+        model = Commission
+        exclude = ['author', 'created_on', 'updated_on']
+
+class JobForm(forms.ModelForm):
+    class Meta:
+        model = Job
+        fields = ['role', 'manpower_required', 'status']
+
+class JobApplicationForm(forms.ModelForm):
+    class Meta:
+        model = JobApplication
+        fields = []
+
+class JobApplicantsForm(forms.ModelForm):
+    class Meta:
+        model = JobApplication
+        fields = ['status']
--- commissions/views.py
+++ commissions/views.py
@@ -22,8 +22,16 @@ def commission_list(request):
     )
 
     commissions = Commission.objects.annotate(status_order=status_order).order_by('status_order', '-created_on')
+
+    ctx = {
+        "created_by_user": created_by_user, 
+        "applied_by_user": applied_by_user,
+        "commission_list": commissions,
+    }
     
-    return render(request, 'commissions/commission_list.html', {"commission_list": commissions, "created_by_user": created_by_user, "applied_by_user": applied_by_user,})
+    return render(request, 'commissions/commission_list.html', ctx)
+
+    #add link to creation of commission 
 
 
 
