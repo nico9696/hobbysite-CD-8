7c1d3a722c37639198c0e8a28987d69cdecc43a9
EnsoSantos <162356579+EnsoSantos@users.noreply.github.com>
2025-05-08 23:46:00 +0800

    made fixes to admin.py

--- commissions/admin.py
+++ commissions/admin.py
@@ -5,18 +5,19 @@ class CommissionAdmin(admin.ModelAdmin):
      model = Commission
      search_fields = ('title', )
      list_display = ('id', 'title','description', 'created_on', 'updated_on', 'status')
+     list_filter = ('status',)
  
 class JobAdmin(admin.ModelAdmin):
      model = Job
      search_fields = ('entry', 'role', 'commission__title')
      list_display = ('id', 'entry', 'commission', 'role', 'people_required', 'created_on', 'updated_on', 'status')
-     list_filter = ('commission', )
+     list_filter = ('commission', 'status',)
 
 class JobApplicationAdmin(admin.ModelAdmin):
      model = JobApplication
-     search_fields = ('job', 'applicant')
+     search_fields = ('job', 'author__display_name')
      list_display = ('id', 'job', 'commission', 'applied_on', 'applicant', 'status')
-     list_filter = ('commission', )
+     list_filter = ('commission', 'job', 'status',)
  
 admin.site.register(Commission, CommissionAdmin)
 admin.site.register(Job, JobAdmin)
