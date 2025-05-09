b2ea280edca876bcd513dbfa01d2b40663e2ec38
EnsoSantos <162356579+EnsoSantos@users.noreply.github.com>
2025-05-09 12:59:35 +0800

    fixed author in models

--- commissions/models.py
+++ commissions/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from user_management.models import Profile
 
 class Commission(models.Model):
-    author =  models.ForeignKey(Profile,on_delete=models.CASCADE)
+    author = models.ForeignKey(Profile, null=True, on_delete=models.SET_NULL, related_name='commissions')
+
     title = models.CharField(max_length=255)
     description = models.TextField()
     STATUS_CHOICES = [
