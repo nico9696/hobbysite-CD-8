acd097e908e7614dff76f3dcfdf56abef2c2c682
EnsoSantos <162356579+EnsoSantos@users.noreply.github.com>
2025-05-09 12:22:04 +0800

    added commission_update view

--- commissions/views.py
+++ commissions/views.py
@@ -56,18 +56,54 @@ def commission_detail(request, commission_id):
 @login_required
 def commission_create(request):
     if request.method == "POST":
-        form = CommissionForm(request.POST)
-        if form.is_valid():
-            commission = form.save(commit=False)
+        commission_form = CommissionForm(request.POST)
+        if commission_form.is_valid():
+            commission = commission_form.save(commit=False)
             commission.author = request.user.profile
             commission.save()
             return redirect('commission_detail', commission_id=commission.id)
     else:
-        form = CommissionForm()
+        commission_form = CommissionForm()
 
     ctx = {
-        'form': form
+        'commission_form': commission_form
     }
 
     return render(request, 'commissions/commission_form.html', ctx)
 
+@login_required
+def commission_update(request, commission_id):
+    commission = Commission.objects.filter(id=commission_id).first()
+    
+    if request.method == 'POST':
+        commission_form = CommissionForm(request.POST, instance=commission)
+        job_form = JobForm(request.POST)
+
+        if commission_form.is_valid():
+            commission_form.save()
+
+            if job_form.is_valid() and any(job_form.cleaned_data.values()):
+                job = job_form.save(commit=False)
+                job.commission = commission
+                job.save()
+
+            jobs = Job.objects.filter(commission=commission)
+            if jobs.exists() and all(job.status == 'Full' for job in jobs):
+                commission.status = 'Full'
+                commission.save()
+
+            return redirect('commission_detail', commission_id=commission.id)
+    else:
+        commission_form = CommissionForm(instance=commission)
+        job_form = JobForm()
+
+    jobs = Job.objects.filter(commission=commission)
+
+    ctx = {
+        'commission_form': commission_form,
+        'job_form': job_form,
+        'jobs': jobs,
+        'commission': commission
+    }
+
+    return render(request, 'commissions/commission_form.html', ctx)
