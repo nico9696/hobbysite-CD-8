864c11699ac29464b3a2f77fc3052ef17092e419
M4L3N4B <caikelvin0315@gmail.com>
2025-05-10 22:12:16 +0800

    Added account for sending emails

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -133,8 +133,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '<Your Gmail address>'
-EMAIL_HOST_PASSWORD = '<Your Gmail password>'
+EMAIL_HOST_USER = 'hobbysite.csci2025@gmail.com' # Sender email address
+EMAIL_HOST_PASSWORD = 'gwmx bzds gwgm brja' # App password provided by Google
 EMAIL_USE_TLS = True
 
 # Media folders
