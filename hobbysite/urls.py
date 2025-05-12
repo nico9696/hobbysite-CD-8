969acabca275783c7df53e201faa41cee1ba0b6f
nico9696 <nicholasbenedictdysy@gmail.com>
2025-05-12 20:25:27 +0800

    made defautl page the login page

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +12,7 @@ urlpatterns = [
     path('wiki/', include('wiki.urls')),
     path('merchstore/', include('merchstore.urls')),
     path('profile/', include('user_management.urls')),
+    path('', RedirectView.as_view(url='/profile/login/', permanent=False)),  # Redirect root URL to profile/login/
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
